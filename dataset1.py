19136c58ffb07d033aaa281df9db41e65af4e227
Hikaru Tokuzato <e195748@ie.u-ryukyu.ac.jp>
2021-10-18 10:12:53 +0900

    fix & add ex1.3

--- dataset1.py
+++ dataset1.py
@@ -2,6 +2,7 @@
 import doctest
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.polynomial import roots
 import pandas as pd
 
 def true_function(x):
@@ -24,6 +25,7 @@ def en1():
     plt.plot(x,y)
 
 def en2():
+    en1()
     min = -1
     max = 1
     num = 0.1
@@ -33,10 +35,29 @@ def en2():
     df = pd.DataFrame(data=x,columns=["観測点"])
     df["真値"] = y
     plt.scatter(x=df["観測点"],y=df["真値"],c="red")
-    en1()
+    return df
 
+
+def en3():
+    df = en2()
+    min = -1
+    max = 1
+    num = 0.1
+    np.random.seed(0)
+    x = (max-min)*np.random.rand(int(((max-min)/num)))-1
+    #平均値を0.0、分散を2.0
+    y = np.random.normal(0,np.sqrt(2),20) / 2 #分散2.0が怪しい？ np.random.normalの値かそのものがおかしいのかな？ or 仕様
+    df["観測値"] = y
+    plt.scatter(x=x,y=y,c="green")
+    print(df)
+
+
+def en4():
+    en3()
+    pass
+    
 def main():
-    en2()
+    en3()
     plt.show()
 if __name__ == "__main__":
     main()
